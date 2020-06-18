fbdeb547f001dde3ed1dc543e0ef8aa310714ee0
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2020-06-18 00:25:43 -0400

    new components(directory, menu-item)

---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MenuItem.scss";
+
+const MenuItem = ({ title, image, size }) => {
+  return (
+    <div className={`menu-item ${size}`} >
+      <div className="background-image" style={{ background: `url(${image})` }} />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
