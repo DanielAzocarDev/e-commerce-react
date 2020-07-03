d48927eec66592ad63a2f3965629c52a7e7006b0
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2020-07-03 17:02:21 -0400

    shop page and preview collection component structural and functional part

--- src/components/previewCollec/PreviewCollec.jsx
+++ src/components/previewCollec/PreviewCollec.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './PreviewCollec.scss'
+import React from "react";
+import "./PreviewCollec.scss";
 
-const PreviewCollec = () => {
+const PreviewCollec = ({ title, items }) => {
   return (
-    <div>
-
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          // el filter es para solo mostrar 4 items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <h2 key={item.id}>{item.name}</h2>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PreviewCollec
+export default PreviewCollec;
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shopdata'
+import React, { Component } from "react";
+import SHOP_DATA from "./shopdata";
+import PreviewCollec from "../../components/previewCollec/PreviewCollec";
 
 export default class shop extends Component {
   constructor(props) {
     super();
 
     this.state = {
-      collections: SHOP_DATA
-    }
+      collections: SHOP_DATA,
+    };
   }
   render() {
+    const { collections } = this.state;
     return (
-      <div>
-        shop!
+      <div className="shop-page">
+        {collections.map(({ id, ...collection }) => (
+          <PreviewCollec key={id} {...collection} />
+        ))}
       </div>
-    )
+    );
   }
 }
